ed8af3e0c463f25ed34e68e3e609de1e80bf6738
Sourav Sadhukhan <ssadhukhan990@gmail.com>
2025-11-23 20:09:45 +0530

    feat: Add new profile image and update personal info data with the new image.

--- src/data/personalInfo.ts
+++ src/data/personalInfo.ts
@@ -3,6 +3,8 @@ import profileImage1 from '../assets/images/profile(1).jpg';
 import profileImage2 from '../assets/images/profile(2).jpg';
 import profileImage3 from '../assets/images/profile(3).png';
 import profileImage4 from '../assets/images/profile(4).jpg';
+import profileImage5 from '../assets/images/profile(5).jpg';
+import profileImage6 from '../assets/images/profile(6).jpg';
 
 // Define the interface for personal information
 export interface PersonalInfo {
@@ -28,7 +30,7 @@ export const personalInfo: PersonalInfo = {
   location: "Kolkata, India",
   email: "ssadhukhan990@gmail.com",
   phone: "+91 9038516950",
-  profileImages: [profileImage1, profileImage2, profileImage3, profileImage4],
+  profileImages: [profileImage1, profileImage2, profileImage3, profileImage4, profileImage5, profileImage6],
   viewResumeLink: 'https://drive.google.com/file/d/1Lzqp5ojWS3CKBkkfRrWacKBGdazQ9Pci/preview',
   downLoadResumeLink: 'https://drive.google.com/uc?export=download&id=1Lzqp5ojWS3CKBkkfRrWacKBGdazQ9Pci'
 };
